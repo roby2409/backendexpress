d77be2acfa6cae4648696c4981cff901171a142c
Roby <robysetiawan2409@gmail.com>
2024-04-02 07:46:18 +0700

    feat(module): user repository
    - add user
    - find user by id
    - update user
    - all user
    - delete user

---
+++ src/repositories/userRepository.ts
@@ -0,0 +1,36 @@
+import prismaClient from '../db/prismaClient';
+import { User } from '../entities/userEntity'
+const prisma = prismaClient.prisma;
+
+export const addUser = (name: string) => {
+    return prisma.user.create({
+        data: {
+            name,
+            point: 100,
+        },
+    });
+};
+
+export const findUserById = (id: number): Promise<User | null> => {
+    return prisma.user.findUnique({
+        where: { id },
+    });
+};
+
+
+export const updateUser = (id: number, data: Record<string, any>) => {
+    return prisma.user.update({
+        where: { id: id },
+        data: { ...data },
+    });
+};
+
+export const allUsers = () => {
+    return prisma.user.findMany();
+}
+
+export const deleteUser = (userId: number) => {
+    return prisma.user.delete({
+        where: { id: userId },
+    });
+};
