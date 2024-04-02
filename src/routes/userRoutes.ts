eb3dc3a1c17b908c9af9e93cc63f65ada7041364
Roby <robysetiawan2409@gmail.com>
2024-04-02 10:36:43 +0700

    refactor: implement auth validation middleware at user routes

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,13 +1,13 @@
 import express from 'express';
 import * as userController from '../controllers/userController';
-
+import { accessValidation } from '../services/authService'
 const router = express.Router();
 
-router.get('/list-of-user', userController.listOfUsers);
-router.get('/user/:id', userController.userById);
-router.put('/user/:id', userController.updateUser)
-router.post('/user', userController.addNewUser)
-router.delete('/user/:id', userController.deleteUser)
+router.get('/list-of-user', accessValidation, userController.listOfUsers);
+router.get('/user/:id', accessValidation, userController.userById);
+router.put('/user/:id', accessValidation, userController.updateUser)
+router.post('/user', accessValidation, userController.addNewUser)
+router.delete('/user/:id', accessValidation, userController.deleteUser)
 
 
 export default router;
